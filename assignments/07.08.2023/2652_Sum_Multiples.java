dd454c79391358bfeac5ebd97f210b147c7f52aa
Dhruv <dhruv.p6@ahduni.edu.in>
2023-08-07 14:43:47 +0530

    v1.000

--- assignments/07.08.2023/2652_Sum_Multiples.java
+++ assignments/07.08.2023/2652_Sum_Multiples.java
@@ -1,12 +1,10 @@
 class Solution {
     public int sumOfMultiples(int n) {
         int sum = 0;
-
         while(n>=1){
-            if(n%3 == 0 || n%5 == 0 || n%7 == 0)sum += n;
+            if(n%7 == 0 || n%5 == 0 || n%3 == 0)sum += n;
             n--;
         }
-
         return sum;
     }
 }
